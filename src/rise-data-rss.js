7b412c20f92b3b4000e6e8a63faa7bdf91b79c23
Francisco Vallarino <fjvallarino@gmail.com>
2019-07-04 17:22:42 -0300

    Add skeleton project

---
+++ src/rise-data-rss.js
@@ -0,0 +1,31 @@
+import { html } from "@polymer/polymer";
+import { RiseElement } from "rise-common-component/src/rise-element.js";
+import { version } from "./rise-data-rss-version.js";
+
+export default class RiseDataRss extends RiseElement {
+
+  static get template() {
+    return html`[[value]]`;
+  }
+
+  static get properties() {
+    return {
+      feedUrl: {
+        type: String
+      },
+      maxItems: {
+        type: Number,
+        value: 25
+      }
+    };
+  }
+
+  constructor() {
+    super();
+
+    this._setVersion( version );
+  }
+
+}
+
+customElements.define("rise-data-rss", RiseDataRss);
