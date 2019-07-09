12eb8fb77be5f52ee2feb6a31213c36d76c272c3
Francisco Vallarino <fjvallarino@gmail.com>
2019-07-09 19:03:48 -0300

    Process response, generate events and logs

--- src/rise-data-rss.js
+++ src/rise-data-rss.js
@@ -11,7 +11,7 @@ export default class RiseDataRss extends RiseElement {
        */
       feedUrl: {
         type: String,
-        observer: "_requestData"
+        observer: "_feedUrlChanged"
       },
       /**
        * The maximum number of items to return from the feed. The maximum allowed is 25.
@@ -30,53 +30,93 @@ export default class RiseDataRss extends RiseElement {
     };
   }
 
+  // Event name constants
+  static get EVENT_DATA_UPDATE() {
+    return "data-update";
+  }
+
+  static get EVENT_DATA_ERROR() {
+    return "data-error";
+  }
+
+  static get EVENT_REQUEST_ERROR() {
+    return "request-error";
+  }
+
   constructor() {
     super();
 
     this._setVersion(version);
-  }
-
-  ready() {
-    super.ready();
-  }
-
-  _init() {
-    super._init();
-  }
-
-  _handleStart() {
-    super._handleStart();
+    this._initialStart = true;
   }
 
   _getUrl() {
     return rssConfig.feedParserURL + "/" + this.feedUrl;
   }
 
+  _feedUrlChanged() {
+    if (!this._initialStart && this.feedUrl) {
+      this._requestData();
+    }
+  }
+
+  _handleStart() {
+    super._handleStart();
+
+    if (this._initialStart) {
+      this._initialStart = false;
+
+      super.log("info", "rss-start");
+
+      if (this.feedUrl) {
+        this._requestData();
+      }
+    }
+  }
+
   _requestData() {
     fetch(this._getUrl(), {
       headers: { "X-Requested-With": "rise-data-rss" }
     })
-    .then(res => {
-      //this._logData( false );
-      this._handleResponse(res.clone());
-      //super.putCache(res);
+    .then(resp => {
+      return resp.json();
     })
-    .catch(() => {
-      this._handleFetchError();
-    });
-  }
-
-  _handleResponse(resp) {
-    resp.json()
     .then(data => {
-      this.feedData = data;
-      super.log("info", "data received", { data });
+      this._handleResponse(data);
     })
     .catch(err => {
-      super.log("error", "data error", { error: err.message });
+      this._handleFetchError(err);
     });
   }
 
+  _handleResponse(data) {
+    console.log('_handleResponse', data);
+    if (!data.Error) {
+      // Temporary solution; this should use a more robust comparison method
+      if (JSON.stringify(this.feedData) !== JSON.stringify(data)) {
+        this._setFeedData(data);
+
+        super.log("info", "rss-data-update", this.feedData);
+        this._sendEvent(RiseDataRss.EVENT_DATA_UPDATE, this.feedData);
+      }
+      else {
+        super.log("info", "rss-data-unchanged");
+      }
+    }
+    else {
+      let error = data.Error;
+
+      super.log("error", "rss-data-error", { error });
+      this._sendEvent(RiseDataRss.EVENT_DATA_ERROR, { error });
+    }
+  }
+
+  _handleFetchError(err) {
+    let error = err ? err.message : null;
+
+    super.log("error", "rss-request-error", { error });
+    this._sendEvent(RiseDataRss.EVENT_REQUEST_ERROR, { error });
+  }
 }
 
 customElements.define("rise-data-rss", RiseDataRss);
