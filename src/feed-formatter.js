5f4d3487bd9d32aa220496779d667773aabe3493
Francisco Vallarino <fjvallarino@gmail.com>
2019-08-28 23:40:07 -0300

    Add feed formatter to unify format of properties of interest

---
+++ src/feed-formatter.js
@@ -0,0 +1,83 @@
+//const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)/;
+const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+
+export default class FeedFormatter {
+  formatFeed (feed) {
+    let formatted = {};
+
+    formatted.title = this._getTitle(feed);
+    formatted.description = this._getDescription(feed);
+    formatted.link = this._getLink(feed);
+    formatted.imageUrl = this._getImageUrl(feed);
+    formatted.author = this._getAuthor(feed);
+    formatted.pubDate = this._getPubDate(feed);
+
+    return formatted;
+  }
+
+  _getTitle (feed) {
+    return feed.title;
+  }
+
+  _getDescription (feed) {
+    if (feed["rss:description"] && feed["rss:description"]["#"]) {
+      return feed["rss:description"]["#"];
+    } else if (feed.summary) {
+      return feed.summary;
+    } else {
+      return feed.description;
+    }
+  }
+
+  _getLink (feed) {
+    return feed.link || feed.permalink;
+  }
+
+  _getImageUrl (feed) {
+    let foundImages = [];
+
+    if (feed["content:encoded"] && feed["content:encoded"]["#"]) {
+      let rgx = new RegExp("src='(" + URL_REGEX.source + ")'");
+      let res = feed["content:encoded"]["#"].match(rgx);
+
+      if (res.length > 1 && res[1]) {
+        foundImages.push(res[1]);
+      }
+    }
+
+    if (feed["rss:image"] && feed["rss:image"]["#"]) {
+      foundImages.push(feed["rss:image"]["#"]);
+    }
+
+    if (feed["media:thumbnail"] && feed["media:thumbnail"]["@"] && feed["media:thumbnail"]["@"].url) {
+      foundImages.push(feed["media:thumbnail"]["@"].url);
+    }
+
+    if (feed.enclosures && feed.enclosures.length > 0 && feed.enclosures[0].url) {
+      foundImages.push(feed.enclosures[0].url);
+    }
+
+    if (feed.image && feed.image.url) {
+      foundImages.push(feed.image.url);
+    }
+
+    return foundImages.find(this._isValidImage);
+  }
+
+  _getAuthor (feed) {
+    return feed.author;
+  }
+
+  _getPubDate (feed) {
+    return feed.pubdate || feed.pubDate;
+  }
+
+  _isValidImage (imageUrl) {
+    let validFormats = [ "bmp", "jpeg", "jpg", "png" ];
+    let url = imageUrl.toLowerCase();
+
+    return !!validFormats.find( ext => {
+      return url.endsWith("." + ext) || url.indexOf("." + ext + "?") >= 0;
+    });
+  }
+}
